f55160fef26dade8879ace95a1b25f425d1b69d7
AkhileshGattu <aghgc@mail.umkc.edu>
2016-02-10 00:24:57 -0600

    Sample commit

---
+++ MapsandCamera/app/src/main/java/com/example/santu_pc/mapsandcamera/Login.java
@@ -0,0 +1,88 @@
+package com.example.santu_pc.mapsandcamera;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity {
+
+    ImageView img;
+    int PHOTO_REQUEST_CODE = 1313;
+
+    public void TakePhoto(View view){
+
+        Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camIntent, PHOTO_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PHOTO_REQUEST_CODE ){
+            Bitmap Takenimg = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(Takenimg);
+        }
+    }
+
+    public void ViewLocation(View view){
+
+        img.setDrawingCacheEnabled(true);
+        Bitmap bm = img.getDrawingCache();
+        Intent mapsIntent = new Intent(Login.this,MapsActivity.class);
+        mapsIntent.putExtra("PROFILEIMG", bm);
+        startActivity(mapsIntent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        img = (ImageView) findViewById(R.id.imageView);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
